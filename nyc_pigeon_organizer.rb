e17b6936507f8d88d50e4a798ad98d4687ece8dc
Serkan Sahin <ssahin55@outlook.com>
2020-08-30 21:39:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_hash|
     value.each do |inner_key, names|
       names.each do |name|
-        # binding.pry
+        binding.pry
         if !new_hash[name]
           new_hash[name] = {}
         end
